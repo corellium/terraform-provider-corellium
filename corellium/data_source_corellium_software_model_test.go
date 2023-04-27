f9fe5ab39f3c6775083fdc1eee6e21bfd978fa4d
Brandon Sanchez <brandon.sanchez@ai.moda>
2023-04-27 15:55:55 -0500

    Added software model endpoint workaround. Updated tests and placehodler ID assignments.

--- corellium/data_source_corellium_software_model_test.go
+++ corellium/data_source_corellium_software_model_test.go
@@ -25,7 +25,22 @@ func TestAccCorelliumV1SofwareDataSource(t *testing.T) {
 			{
 				Config: providerConfig + testAccCorelliumV1SoftwareDataSourceConfig("iPhone15,3"),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("corellium_v1modelsoftware.test", "model", "iPhone15,3"),
+					// Verify the first model to ensure all attributes are set
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model", "iPhone15,3"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.version", "16.0"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.build_id", "20A362"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.sha256_sum", "4f12cc262aa87647bebc83a6ca0ae29bd11dff2ad73812cc45d920be4caaa584"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.sha1_sum", ""),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.md5_sum", "fa07e00024db4a50a132cd4ba7575c10-788"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.size", "6609575844"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.unique_id", ""),
+					// resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.metadata", ""),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.release_date", "2022-09-12T17:01:06Z"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.upload_date", ""),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.url", "https://updates.cdn-apple.com/2022FallFCS/fullrestores/012-65663/2051812C-0862-4EA6-A896-365466C2DBAD/iPhone15,3_16.0_20A362_Restore.ipsw"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.orig_url", "https://updates.cdn-apple.com/2022FallFCS/fullrestores/012-65663/2051812C-0862-4EA6-A896-365466C2DBAD/iPhone15,3_16.0_20A362_Restore.ipsw"),
+					resource.TestCheckResourceAttr("data.corellium_v1modelsoftware.test", "model_software.0.filename", "iPhone15,3_16.0_20A362_Restore.ipsw"),
+					resource.TestCheckResourceAttrSet("data.corellium_v1modelsoftware.test", "id"),
 				),
 			},
 		},
