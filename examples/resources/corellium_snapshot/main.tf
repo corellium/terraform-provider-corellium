03b30ea24b3ff35bf7856a3e9f53286cc6132109
Henry Barreto <me@henrybarreto.dev>
2023-05-20 17:37:45 -0300

    run terraform fmt on examples and changed student string to demo on demo.tf

--- examples/resources/corellium_snapshot/main.tf
+++ examples/resources/corellium_snapshot/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     corellium = {
-      source = "github.com/aimoda/corellium"
+      source  = "github.com/aimoda/corellium"
       version = "~> 1.0.0"
     }
   }
@@ -17,28 +17,28 @@ provider "corellium" {
 resource "corellium_v1project" "example" {
   name = "example"
   settings = {
-      version = 1
-      internet_access = false
-      dhcp = false
+    version         = 1
+    internet_access = false
+    dhcp            = false
   }
   quotas = {
-      cores = 2
+    cores = 2
   }
   teams = []
   users = []
 }
 
 resource "corellium_v1instance" "example" {
-  name = "example"
-  flavor = "iphone7plus"
-  os = "15.7.5"
-  project = corellium_v1project.example.id
-  wait_for_ready = true
+  name                   = "example"
+  flavor                 = "iphone7plus"
+  os                     = "15.7.5"
+  project                = corellium_v1project.example.id
+  wait_for_ready         = true
   wait_for_ready_timeout = 500
 }
 
 resource "corellium_v1snapshot" "example" {
-  name = "example"
+  name     = "example"
   instance = corellium_v1instance.example.id
 }
 
--- examples/resources/corellium_webplayer/main.tf
+++ examples/resources/corellium_webplayer/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     corellium = {
-      source = "github.com/aimoda/corellium"
+      source  = "github.com/aimoda/corellium"
       version = "~> 1.0.0"
     }
   }
@@ -17,47 +17,47 @@ provider "corellium" {
 resource "corellium_v1project" "example" {
   name = "example"
   settings = {
-    version = 1
+    version         = 1
     internet_access = false
-    dhcp = false
+    dhcp            = false
   }
   quotas = {
-      cores = 2
+    cores = 2
   }
   users = []
   teams = []
 }
 
 resource "corellium_v1instance" "example" {
-  name = "example"
-  flavor = "ranchu"
+  name    = "example"
+  flavor  = "ranchu"
   project = corellium_v1project.example.id
-  os = "7.1.2"
+  os      = "7.1.2"
 }
 
 resource "corellium_v1webplayer" "example" {
-  project = corellium_v1project.example.id
-  instanceid = corellium_v1instance.example.id
+  project          = corellium_v1project.example.id
+  instanceid       = corellium_v1instance.example.id
   expiresinseconds = "1800"
   features = {
-    apps = false
-    console = false
-    coretrace = false
-    devicecontrol = false
-    devicedelete = false
-    files = false
-    frida = false
-    images = false
-    messaging = false
-    netmon = false
-    network = false
+    apps           = false
+    console        = false
+    coretrace      = false
+    devicecontrol  = false
+    devicedelete   = false
+    files          = false
+    frida          = false
+    images         = false
+    messaging      = false
+    netmon         = false
+    network        = false
     portforwarding = false
-    profile = false
-    sensors = false
-    settings = false
-    snapshots = false
-    strace = false
-    system = false
-    connect = false
-}
+    profile        = false
+    sensors        = false
+    settings       = false
+    snapshots      = false
+    strace         = false
+    system         = false
+    connect        = false
+  }
 }
